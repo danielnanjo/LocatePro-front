732bf9207029889c73ad548b4dac045f64ac7119
danielnanjo <ndangsananjodaniel@gmail.com>
2025-08-28 04:10:56 -0700

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000'
+const API_BASE = import.meta.env.VITE_API_BASE || 'https://locatepro-back.onrender.com'
 
 export const api = axios.create({ baseURL: API_BASE })
 
