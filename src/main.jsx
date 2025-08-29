2a34b39b262ca9fb5b4cdd7e575629351fc1e340
danielnanjo <ndangsananjodaniel@gmail.com>
2025-08-29 09:08:45 -0700

    chat

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
@@ -34,3 +35,19 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
+
+export default function Home() {
+  useEffect(() => {
+    // Only add script if not already present
+    if (!window.Tawk_API) {
+      const s1 = document.createElement("script");
+      s1.async = true;
+      s1.src = 'https://embed.tawk.to/68a924307d18e21931dd2518/1j3abao74';
+      s1.charset = 'UTF-8';
+      s1.setAttribute('crossorigin', '*');
+      document.body.appendChild(s1);
+    }
+  }, []);
+
+  // ...existing code...
+}
